19ba78da5d9170fbb3ee3f08c924014ca44b9b48
prietomarcelo40 <prietomarcelo40@gmail.com>
2025-08-08 18:33:42 -0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def contacto():
     return render_template('contacto.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
