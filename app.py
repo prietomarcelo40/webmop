b07b40a7929077ffa59190455ca8ef9def3a7074
prietomarcelo40 <prietomarcelo40@gmail.com>
2025-08-12 20:44:09 -0300

    Actualización completa: app.py, templates, css y estructura

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def inicio():
@@ -23,4 +23,4 @@ def contacto():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
